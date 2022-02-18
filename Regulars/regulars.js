7082baa9cc62f5a05d0e34db083f0bd7dd6a627b
fabarm <leonovxai@gmail.com>
2022-02-18 13:48:31 +0200

    complete

--- Regulars/regulars.js
+++ Regulars/regulars.js
@@ -9,8 +9,8 @@ let regPhone = /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
 
 let regEmail = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
 
-let regSite = /http\:\/\/test\.dev/;
+let regSite = /^http\:\/\/test\.dev$/;
 
 let regPasword = /^[a-z0-9_]{6,25}$/;
 
-let regIPV4 = /^ /g;
+let regIPV4 = /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b/;
